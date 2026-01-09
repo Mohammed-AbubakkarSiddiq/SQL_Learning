931dd75d64fa28c69413deaa7bf2395fe1b1ca8c
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-01-09 18:05:20 +0530

    updated

--- MS_SQL_Server/SQLQueryLearning.sql
+++ MS_SQL_Server/SQLQueryLearning.sql
@@ -1,3 +1,5 @@
+use BikeStores;
+
 --Crearing a database
 create database BikeStores
 
@@ -12,3 +14,29 @@ GO -- Separator  for running multiple queries
 --Dropping a database
 DROP DATABASE Dummy;
 GO
+
+-- Creating a table
+
+create table production.pokemon(
+
+id int identity(1,1) primary key,
+name varchar(255) not null,
+phone varchar(25),
+email varchar(255)
+
+);
+
+-- Creating table with foreigh key
+
+create table production.trainer (
+
+id int identity(1,1) primary key,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+email varchar(255) not null unique,
+phone varchar(40) not null, 
+poke_id int not null
+foreign key (poke_id)
+	references production.pokemon (id)
+	on delete cascade on update cascade
+);
